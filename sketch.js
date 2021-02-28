d7f3cc11cfff279d289da24bf466c894cf96c6e8
Prasanth A R <raviprasanth45@gmail.com>
2021-02-28 21:02:57 +0530

    Added with animation

--- sketch.js
+++ sketch.js
@@ -1,21 +1,40 @@
+let x, y;
+let a = 0;
+// let heart = createVector(300, 300);
+let heartx = [];
+let hearty = [];
 function setup()
 {
-    createCanvas(500, 500);
+    createCanvas(720, 480);
 }
-let x, y;
 function draw()
 {
     background(0);
     translate(width/2, height/2);
-    noFill();
+    // fill(255, 255,0);
+    drawHeart();
+    // translate(width/2, height-100);
+    // drawHeart();
+    // drawHeart();
+    // setTimeout(drawHeart, 1000);
+    
+    // console.log(heartx, hearty);
+}
+
+function drawHeart()
+{
+    let r = 10;
+    x = r *  16 * pow(sin(a), 3);
+    y = -r * (15 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+    heartx.push(x);
+    hearty.push(y);
+    fill(150, 0, 255);
     stroke(255);
     beginShape();
-    for(let a=0; a<=TWO_PI; a+=0.01)
+    for(let i =0;i<=heartx.length; i+=1)
     {
-        let r = 10;
-        x = r *  16 * pow(sin(a), 3);
-        y = -r * (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
-        vertex(x, y);
+        ellipse(heartx[i], hearty[i], 3);
     }
     endShape();
+    a += 0.03;
 }
